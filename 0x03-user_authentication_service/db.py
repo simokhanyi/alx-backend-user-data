5135cfe15b120ea6a3eba96753783362ebf9eeb4
Simo Khanyi <simokhanyi@gmail.com>
2024-06-12 14:19:41 +0000

    User authentication service

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -8,6 +8,7 @@ from sqlalchemy.orm.session import Session
 
 from user import Base, User
 
+
 class DB:
     """DB class
     """
@@ -31,7 +32,7 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """Add a new user to the database.
-        
+
         Args:
             email (str): The email of the user.
             hashed_password (str): The hashed password of the user.
--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -20,9 +20,9 @@ class User(Base):
         session_id (str): The session ID of the user, nullable.
         reset_token (str): The reset token for password recovery, nullable.
     """
-    
+
     __tablename__ = 'users'
-    
+
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
